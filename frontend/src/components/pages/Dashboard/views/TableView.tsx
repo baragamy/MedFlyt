915482ab8a05b4e94cfbbbe44401a760d939bd23
Bar Agamy <bar.agamy@gmail.com>
2021-06-29 00:13:08 +0300

    fix bug frontend+server

--- frontend/src/components/pages/Dashboard/views/TableView.tsx
+++ frontend/src/components/pages/Dashboard/views/TableView.tsx
@@ -14,7 +14,9 @@ import { Report } from "../Dashboard";
 interface Props {
     report: Report;
     isRefreshing: boolean;
+    onClickRefresh: () => void;
 }
+
 
 const Header = styled(Center)`
     height: auto;
@@ -45,7 +47,7 @@ const TableView = (props: Props) => {
                 <PrimaryText>Year {props.report.year} - caregivers report</PrimaryText>
             </Header>
             <Row justifyContent="flex-end">
-                <Button>
+                <Button onClick={props.onClickRefresh}>
                     <RefreshIcon />
                     <span>Refresh</span>
                 </Button>
